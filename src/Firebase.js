c67ca536721b1f613e54ef7f4fb3a4c94ed14ffb
Kauan Reis <kauanpedroreis4@gmail.com>
2023-08-28 23:12:40 -0300

    feat: added environment variables environment variables for firebase authentication

---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: __FIREBASE_API_KEY__,
+  authDomain: __FIREBASE_AUTH_DOMAIN__,
+  projectId: __FIREBASE_PROJECT_ID__,
+  storageBucket: __FIREBASE_STORAGE_BUCKET__,
+  messagingSenderId: __FIREBASE_MESSAGING_SENDER_ID__,
+  appId: __FIREBASE_APP_ID__,
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const authFirebase = getAuth(app)
